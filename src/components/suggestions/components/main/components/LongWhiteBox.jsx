0e0d02a5db7d3411954aaa7998854af94ff8330e
Fabian Andiel <fabian.andiel@yahoo.com>
2021-07-29 13:45:50 +0200

    Styling long white box

--- src/components/suggestions/components/main/components/LongWhiteBox.jsx
+++ src/components/suggestions/components/main/components/LongWhiteBox.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import * as LongWhiteBoxStyles from "LongWhiteBox.module.scss";
+import * as LongWhiteBoxStyles from "./LongWhiteBox.module.scss";
+import Pill from '../../sidebar/components/pill';
 
 function LongWhiteBox() {
-    return (
-        <div className="LongWhiteBox">
-            <div className="counter">
 
+    const SpeechBubble = <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fill-rule="nonzero"/></svg>;
+
+    return (
+        <div className={LongWhiteBoxStyles.LongWhiteBox}>
+            
+            <div className={LongWhiteBoxStyles.LongWhiteBox__firstFlexItem}>
+                <div className="counter">
+                     counter goes here
+                </div>
+                <div className={LongWhiteBoxStyles.LongWhiteBox__firstFlexItem__text}>
+                    <h3>Add Text</h3>
+                    <p>Easier to search</p>
+                    <Pill text={"really cool"}/>
+                </div>   
             </div>
-            <div className="text">
-            
-            </div>   
-            <div className="text">
-            
-            </div>            
+
+            <div className={LongWhiteBoxStyles.LongWhiteBox__secondFlexItem}>
+                <div className={LongWhiteBoxStyles.LongWhiteBox__secondFlexItem__container}>
+                    {SpeechBubble}
+                    <div>number</div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/suggestions/components/main/main.jsx
+++ src/components/suggestions/components/main/main.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
+import LongWhiteBox from './components/LongWhiteBox';
+
 
 const Main = () => {
     return (
 
-        <p>
-        Sort by:
-        Most upvotes
-        Least upvotes
-        Most comments
-        Least comments
-      
-        Add Feedback
-      
-   
-        There is no feedback yet. Got a suggestion? Found a bug that needs to be squashed? We love 
-        hearing about new ideas to improve our app.
-      
-        Add Feedback
-      
-      
-          </p>
+        <>
+        
+            <p>
+            Sort by:
+            Most upvotes
+            Least upvotes
+            Most comments
+            Least comments
+        
+            Add Feedback
+        
+    
+            There is no feedback yet. Got a suggestion? Found a bug that needs to be squashed? We love 
+            hearing about new ideas to improve our app.
+        
+            Add Feedback
+
+            </p>
+            
+            <LongWhiteBox/>
+        </>
     )
 }
 
