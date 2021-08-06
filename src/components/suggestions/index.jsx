982122a3b69e8d71aba628077850c65cb20023c0
Fabian Andiel <fabian.andiel@yahoo.com>
2021-08-06 09:29:53 +0200

    moved data fetch to main suggestions component

--- src/components/suggestions/index.jsx
+++ src/components/suggestions/index.jsx
@@ -3,16 +3,19 @@ import Main from './components/main/main'
 import Sidebar from './components/sidebar/sidebar';
 import * as suggestionStyles from './suggestions.module.scss';
 import Menubar from './components/menubar/menubar';
+import Data from "../../assets/data.json";
 
 const Suggestions = () => {
+    const {productRequests} = Data;
+
     return (
         <div className={suggestionStyles.suggestionsContainer}>
             <div className={suggestionStyles.suggestionsContainer__sidebar}>
                 <Sidebar/>
             </div>
             <div className={suggestionStyles.suggestionsContainer__main}>
-                <Menubar/>  
-                <Main/>
+                <Menubar data={productRequests.length}/>  
+                <Main data={productRequests}/>
             </div>
         </div> 
     )
