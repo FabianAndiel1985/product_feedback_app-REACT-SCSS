24c1fb6525f5be3fe3d0959ffdb22f02b1438a85
Fabian Andiel <fabian.andiel@yahoo.com>
2021-08-27 13:16:26 +0200

    outline of feedbackdetail

--- src/components/feedback/FeedbackDetail.jsx
+++ src/components/feedback/FeedbackDetail.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import CustomButton from '../shared/button';
+import * as FeedbackDetailStyles from './styles.module.scss';
+import LongWhiteBox from '../suggestions/components/main/components/LongWhiteBox'
 
 function FeedbackDetail() {
+
+    const leftArrow = <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L2 5l4-4" stroke="#4661E6" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>;
+
     return (
         <>
-            <p>im the detail component</p>
+            <div className={FeedbackDetailStyles.firstLine}>
+                <p> {leftArrow} Go Back</p> 
+                <CustomButton 
+                    text={"Edit Feedback"}
+                    color={"#4661E6"}
+                    hoverColor={"#c75af6"}
+                /> 
+            </div>
+
+            <LongWhiteBox/>
+
+
+            Komponente für Beitrag
+
+
+            Kommentare
+
+
+    Reply
+
+
+
+  Add Comment
+  Type your comment here
+  250 Characters left
+
+  Post Comment
         </>
     )
 }
