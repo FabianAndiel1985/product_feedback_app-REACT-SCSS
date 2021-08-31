3758bacdd95aca88a6c0333ea5ac92fba4a28b02
Fabian Andiel <fabian.andiel@yahoo.com>
2021-08-31 11:13:04 +0200

    almost finished comments

--- src/components/feedback/components/commentsService.jsx
+++ src/components/feedback/components/commentsService.jsx
@@ -2,21 +2,20 @@ import Comment from "./components/comment";
 
 
 export const createComments = 
-(comments)=> {  return(
+(comments )=> {  return(
         [...comments].map(comment=> {
-
             if(comment && !comment.replies ) {
                 return(
-                    <div>
+                    <div style={comment.replyingTo ? {marginLeft:"80px"} : {}} >
                         <Comment comment={comment} replyingTo={comment.replyingTo}/>
-                        <hr/>
+                            { !comment.replyingTo ? <hr/> : " "}
                     </div>
                 )
             }
 
             else if(comment && comment.replies && comment.replies.length >0) {
                 let replies = comment.replies;
-                console.log(replies)
+            
                 return(
                     <div>
                         <Comment comment={comment}/>
